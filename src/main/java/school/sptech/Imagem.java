4f9f48a96d1ffa834a0d2fda2d186ff3957f820e
Pablo Haddad <pablo.silva@sptech.school>
2024-11-07 18:24:55 -0300

    Subindo exercicio

--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,42 @@
 package school.sptech;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imagem {
+
+    private List<Figura> figuras;
+
+    public void adicionar(Figura figura){
+        figuras.add(figura);
+    }
+    public Double calcularSomaDasAreas(){
+        Double somaDasAreas = 0.0;
+        for (Figura figura : figuras){
+            somaDasAreas += figura.calcularArea();
+        }
+        return somaDasAreas;
+    }
+    public List<Figura> buscarPorAreaMaiorQue20(){
+        List<Figura> maiorQueVinte = new ArrayList<>();
+
+
+        for (Figura figura : figuras){
+            if(figura.calcularArea() > 20.0){
+                maiorQueVinte.add(figura);
+            }
+        }
+        return maiorQueVinte;
+    }
+    public List<Figura> buscarQuadrados(){
+        List<Figura> soQuadrado = new ArrayList<>();
+
+        for (Figura figura : figuras){
+            if (figura instanceof Quadrado){
+                soQuadrado.add(figura);
+            }
+        }
+        return soQuadrado;
+    }
+
 }
